6157f34ee804791b5d8de123601931402678b616
Arkady <orkasha@gmail.com>
2024-12-16 01:29:57 +0200

    build UTXO

--- src/main/java/org/am/com/blockchainnode/controller/NodeController.java
+++ src/main/java/org/am/com/blockchainnode/controller/NodeController.java
@@ -1,9 +1,9 @@
 package org.am.com.blockchainnode.controller;
 
-import org.am.com.blockchainnode.MempoolService;
 import org.am.com.blockchainnode.domain.block.Block;
-import org.am.com.blockchainnode.domain.TransactionRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Date;
