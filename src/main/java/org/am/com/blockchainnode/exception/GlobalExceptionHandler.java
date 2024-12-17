314e6f46fdf3ef16e2f599c578cce00afc822c93
Arkady <orkasha@gmail.com>
2024-12-17 23:42:53 +0200

    rename domain to model

--- src/main/java/org/am/com/blockchainnode/exception/GlobalExceptionHandler.java
+++ src/main/java/org/am/com/blockchainnode/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package org.am.com.blockchainnode.controller;
+package org.am.com.blockchainnode.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
