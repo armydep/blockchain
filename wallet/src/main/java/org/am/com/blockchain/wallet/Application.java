ca2020d638b21173b3f5067350c68bdb5f6d2c2f
arkady <orkasha@gmail.com>
2024-12-24 18:25:01 +0200

    multiple modules init

---
+++ wallet/src/main/java/org/am/com/blockchain/wallet/Application.java
@@ -0,0 +1,15 @@
+package org.am.com.blockchain.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
