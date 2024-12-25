408c4c6f82bd168b9115fb924b69d1b99ad4a67e
Arkady <orkasha@gmail.com>
2024-12-25 22:56:57 +0200

    format

--- wallet/src/main/java/org/am/com/blockchain/wallet/model/User.java
+++ wallet/src/main/java/org/am/com/blockchain/wallet/model/User.java
@@ -2,8 +2,9 @@ package org.am.com.blockchain.wallet.model;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
-
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -11,10 +12,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
-
     private String password;
+    private String address;
 
     public User() {
     }
@@ -22,29 +22,5 @@ public class User {
     public User(String user1, String password1) {
         this.username = user1;
         this.password = password1;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
