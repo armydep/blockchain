5a94b6c2d8efe94f6e1b28b209a683eb89587023
Arkady <orkasha@gmail.com>
2025-02-17 18:21:04 +0200

    cors domain added for frontend url

--- wallet/src/main/java/am/com/blockchain/wallet/controller/web/LoginWebController.java
+++ wallet/src/main/java/am/com/blockchain/wallet/controller/web/LoginWebController.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Validated
 @Controller
+@RequestMapping("/web")
 public class LoginWebController {
 
     @GetMapping("/login")
